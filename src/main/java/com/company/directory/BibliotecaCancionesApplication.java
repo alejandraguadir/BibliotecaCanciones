b23c35e0da350a877daa7d3bdb3632ef305c656a
Alejandra Guadir <guadir.alejandra@gmail.com>
2023-01-03 19:19:46 -0500

    first commit

---
+++ src/main/java/com/company/directory/BibliotecaCancionesApplication.java
@@ -0,0 +1,13 @@
+package com.company.directory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BibliotecaCancionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BibliotecaCancionesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/company/directory/BibliotecaCancionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.company.directory;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BibliotecaCancionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
